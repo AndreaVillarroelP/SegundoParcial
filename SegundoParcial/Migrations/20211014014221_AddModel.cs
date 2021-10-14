b1344f0ee7690282cee966749eef4a230ebf0ea2
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-10-13 22:05:01 -0400

    Se crearon las tablas

---
+++ SegundoParcial/Migrations/20211014014221_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SegundoParcial.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Magic",
+                columns: table => new
+                {
+                    FuturoId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Vision = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Magic", x => x.FuturoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Magic");
+        }
+    }
+}
