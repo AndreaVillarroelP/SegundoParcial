ab80145d127775ca74ecdb1fc64b1eaa0d1aa6a0
Andrea <a2020122545@estudiantes.upsa.edu.bo>
2021-10-13 21:11:15 -0400

    Carpeta Models agregada

---
+++ SegundoParcial/Models/Magic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Models
+{
+    public class Magic
+    {
+        [Key]
+        public string FuturoId { get; set; }
+        [Required(ErrorMessage ="EL campo es requerido")]
+        [StringLength(60,MinimumLength =10,ErrorMessage ="Deben haber entre 10 y 60 caracteres")]
+        [Display(Name = "Vision")]
+        public string Vision { get; set; }
+        [Url]
+        [Required(ErrorMessage = "EL campo es requerido")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Deben haber entre 10 y 1000 caracteres")]
+        [Display(Name = "Imagen de la suerte")]
+        public string Imagen { get; set; }
+    }
+}
